5f4c91e5536ecacc7a6dc2384df6112163b0ebf4
Jin Young Kim <wlsdud66523@kakao.com>
2023-05-19 13:24:56 +0900

    Spring Data Jpa Paging Application3

--- src/main/java/study/querydsl/controller/MemberController.java
+++ src/main/java/study/querydsl/controller/MemberController.java
@@ -2,6 +2,8 @@ package study.querydsl.controller;
 
 
 import lombok.RequiredArgsConstructor;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 import study.querydsl.dto.MemberSearchCondition;
@@ -9,7 +11,7 @@ import study.querydsl.dto.MemberTeamDto;
 import study.querydsl.repository.MemberJpaRepository;
 import study.querydsl.repository.MemberRepository;
 
-import java.awt.print.Pageable;
+
 import java.util.List;
 
 @RestController
@@ -24,6 +26,18 @@ public class MemberController {
     }
 
 
+    @GetMapping("/v2/members")
+    public Page<MemberTeamDto> searchMemberV2(MemberSearchCondition condition, Pageable pageable){
 
+        return memberRepository.searchPageSimple(condition,pageable);
+
+    }
+
+    @GetMapping("/v3/members")
+    public Page<MemberTeamDto> searchMemberV3(MemberSearchCondition condition, Pageable pageable){
+
+        return memberRepository.searchPageComplex1(condition,pageable);
+
+    }
 
 }
